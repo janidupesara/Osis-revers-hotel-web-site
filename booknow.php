39f21026818b991cb89b623e6ffb0beea8e5cfce
W.Janidu Pesara <janidu.pesara.work@gmail.com>
2026-05-26 09:15:28 +0530

    add boknow.php

---
+++ booknow.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Oasis Revers Hotel</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  
+    <div class="book">
+        <form>
+            <br>
+            <h1> BOOK NOW</h1>
+            <br>
+            <label for="name">1. full name</label><br>
+            <input type="text" id="name" name="full name" required placeholder="enter the full name"><hr>
+
+            <br>
+            <label for="idnum">2. ID number</label><br>
+            <input type="text" id="ID" name="ID" required placeholder="enter your ID number"><hr>
+
+            <br>
+            <label for="phone num">3. phone number</label><br>
+            <input type="text" id="phone number" name="phone number" required placeholder="enter ther phone number" ><hr>
+
+            <br>
+            <label for="package">4. package</label><br>
+            <select name="package" id="package">
+                <option value="1"> special wedin package</option>
+                <option value="2"> special room offers</option>
+                <option value="2"> weekend buffert</option>
+            </select>
+
+            <button type="submit">submit</button>
+
+        </form>
+    </div>
+
+     <div class="back">
+        <a href="about.html">back</a>
+    </div>
+    
+
+
+</body>
+</html>
